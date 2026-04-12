872aeb6d49b4c105d238b50e1ae9bd7f8323ee91
Vineeth <srinagasrivineeth@gmail.com>
2026-04-12 16:35:40 +0530

    env changes

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,34 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private apiUrl = 'https://localhost:7058/api/Authentication/login';
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
   login(data: any) {
-    return this.http.post(this.apiUrl, data);
-  }
+  return this.http.post(`${this.baseUrl}/Authentication/login`, data);
+}
 
   getEmployees(page: number, size: number) {
   return this.http.get(
-    `https://localhost:7058/api/employee?pageNumber=${page}&pageSize=${size}`
+    `${this.baseUrl}/employee?pageNumber=${page}&pageSize=${size}`
   );
 }
 addEmployee(data: any) {
-  console.log('Adding employee with data:', data);
-  return this.http.post('https://localhost:7058/api/employee', data);
+  return this.http.post(`${this.baseUrl}/employee`, data);
 }
 deleteEmployee(id: number) {
-  console.log('Deleting employee with id:', id);
-  return this.http.delete(`https://localhost:7058/api/employee/${id}`);
+  return this.http.delete(`${this.baseUrl}/employee/${id}`);
 }
 updateEmployee(id: number, data: any) {
-  console.log('Updating employee with id:', id, 'and data:', data);
-  return this.http.put(`https://localhost:7058/api/employee/${id}`, data);
+  return this.http.put(`${this.baseUrl}/employee/${id}`, data);
 }
 }
